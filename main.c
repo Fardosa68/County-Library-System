1a598a1bf171917692d41251b583f4989796553a
Fardosa68 <98381973+Fardosa68@users.noreply.github.com>
2022-02-22 15:42:43 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+A County Library Management System
+Created by Hassan
+on Feb, 2022
+C89 Compiler
+MIT License
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   printf("\tcounty library\n");
+    printf("Welcome Miss. Hassan. \n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user\n");
+    printf("1. Add Book.\n");
+    printf("Selected action: ");
+    return 0;
+}
